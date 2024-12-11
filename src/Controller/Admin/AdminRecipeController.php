89e9d90318e21a92f8330b5df32f4e5b013f675c
David Robert <contact@david-robert.fr>
2024-12-11 12:21:12 +0100

    submit form

--- src/Controller/Admin/AdminRecipeController.php
+++ src/Controller/Admin/AdminRecipeController.php
@@ -4,19 +4,32 @@ namespace App\Controller\Admin;
 
 use App\Entity\Recipe;
 use App\Form\AdminRecipeType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminRecipeController extends AbstractController
 {
 
 
-    #[Route('/admin/recipe/create', 'admin_create_recipe', methods: ['GET'])]
-    public function createRecipe()
+    #[Route('/admin/recipe/create', 'admin_create_recipe', methods: ['GET', 'POST'])]
+    public function createRecipe(Request $request, EntityManagerInterface $entityManager)
     {
         $recipe = new Recipe();
 
         $adminRecipeForm = $this->createForm(AdminRecipeType::class, $recipe);
+
+        // le handleRequest récupère les données de POST (donc du form envoyé)
+        // pour chaque donnée, il va modifier l'entité (setTitle, setImage etc)
+        // et donc remplir l'entité avec les données du form
+        $adminRecipeForm->handleRequest($request);
+
+        if ($adminRecipeForm->isSubmitted()) {
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+        }
+
 
         $adminRecipeFormView = $adminRecipeForm->createView();
 
--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -32,6 +32,11 @@ class Recipe
     #[ORM\ManyToOne(inversedBy: 'recipes')]
     private ?Category $category = null;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
