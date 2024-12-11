423d241aa2faf0d139b17d60b0ce3b5c92ee4115
David Robert <contact@david-robert.fr>
2024-12-11 10:23:38 +0100

    create project + entities

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+
+
+    #[Route('/', 'homepage', methods: ['GET'])]
+    public function showHomePage() {
+
+        return $this->render('homepage.html.twig');
+
+    }
+
+}
