a158fe64dc0c83555f6661f05f8a25a63056916f
David Robert <contact@david-robert.fr>
2024-12-12 15:48:59 +0100

    redirect after login

---
+++ src/Controller/Admin/AdminDashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminDashboardController extends AbstractController
+{
+
+    #[Route('/admin', 'admin_dashboard')]
+    public function dashboard() {
+
+        return $this->render('admin/dashboard.html.twig');
+
+    }
+
+}
