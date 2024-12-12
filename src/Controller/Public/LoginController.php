ba89454319b5c17c45690c30f008cd11ffc165d5
David Robert <contact@david-robert.fr>
2024-12-12 14:55:56 +0100

    login

---
+++ src/Controller/Public/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Public;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+
+    #[Route('/login', 'login')]
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('public/login.html.twig', [
+            'error' => $error,
+            'lastUsername' => $lastUsername
+        ]);
+
+    }
+
+}
